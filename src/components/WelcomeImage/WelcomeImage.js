8a9b9cfcb55a30c050805a588bbe59f9e6241942
WassimZ <zwassim.xeno@gmail.com>
2025-01-26 10:58:04 +0100

    use AND operator

--- src/components/WelcomeImage/WelcomeImage.js
+++ src/components/WelcomeImage/WelcomeImage.js
@@ -4,7 +4,7 @@ import "./WelcomeImage.css";
 import {firstName} from "../../App";
 
 const WelcomeImage = () => {
-    return firstName ? <Image src={imageFile} className={"welcome-image"} alt="welcome image" rounded/> : "";
+    return firstName && <Image src={imageFile} className={"welcome-image"} alt="welcome image" rounded/>;
 }
 
 export default WelcomeImage;
